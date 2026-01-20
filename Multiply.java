6f87deca8cbb946195ec66998048411b8849c588
ReemaDesle <desle.reema@gmail.com>
2026-01-20 14:48:29 +0530

    Commit changes from git

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        // The result of a * b is stored in the 'product' variable
+        int product = a * b; 
+
+        System.out.println("The product of " + a + " and " + b + " is: " + product);
+    }
+}
