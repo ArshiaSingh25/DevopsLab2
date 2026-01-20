323ecaacb49e55f684e07806a5baa700469331cf
ashleshachaware <ashlesha.chaware@cumminscollege.in>
2026-01-20 14:49:34 +0530

    pushing subtraction code

---
+++ subtract.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class subtract{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number");
+        int b = sc.nextInt();
+
+        int res = a - b;
+        System.out.println("Result = " + res);
+        
+        sc.close();
+    }
+}
